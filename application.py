1b6002712eee91b3fbd586777bd9dbe6be86817a
jgritmire <jgritmire@gmail.com>
2023-04-06 10:45:52 -0700

    'test_2'

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 from flask import Flask
+import pandas as pd
 application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return 'Sup'
+    df = pd.read_csv('./data/recommender_data/product_based_recommender_df.csv')
+    return str(df.head())
