e83cde0797d7caecb2b32c6acdab727fd194f503
jgritmire <jgritmire@gmail.com>
2023-04-06 11:20:30 -0700

    'added_more_complexity'

--- application.py
+++ application.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash
 import pandas as pd
 application = Flask(__name__)
+application.secret_key = "password"
 
 @application.route('/')
-def hello_world():
-    df = pd.read_csv('./data/recommender_data/product_based_recommender_df.csv')
-    return str(df.head())
+@application.route("/hello")
+def index():
+    flash("Please enter the names of 3 games separated by backslashes (\)")
+    return render_template("index.html")
+@application.route("/greet", methods=["POST", "GET"])
+def greet():
+    flash("Hi " + str(request.form['name_input']) +", great to see you!")
+    return render_template("index.html")
